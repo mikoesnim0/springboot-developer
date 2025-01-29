ad838606cc82b20eed4c8141d06e065e9d3fc85c
mikoesnim0 <mikoesnim02@gmail.com>
2025-01-29 18:57:09 +0900

    new commit

---
+++ src/main/java/org/edusync/tutor/controller/TutorController.java
@@ -0,0 +1,23 @@
+package org.edusync.tutor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TutorController {
+    @GetMapping(path="/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping(path="/echo/{message}")
+    public String echo(
+            @PathVariable String message
+    ) {
+        System.out.println(message + message);
+        return message;
+    }
+}
---
+++ src/test/java/org/edusync/tutor/TutorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edusync.tutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
