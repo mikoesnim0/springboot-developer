aef3c04c681bac1ded414e95f48e2228ec8b8ac8
mikoesnim0 <mikoesnim02@gmail.com>
2025-01-29 18:59:20 +0900

    cursor settings with html get method

--- src/main/java/org/edusync/tutor/controller/TutorController.java
+++ src/main/java/org/edusync/tutor/controller/TutorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TutorController {
     @GetMapping(path="/hello")
     public String hello() {
-        return "Hello World";
+        return "<html><body><h1>Hello World</h1></body></html>";
     }
 
     @GetMapping(path="/echo/{message}")
